73478caae78a8d582a691e59cc20a1c308e7396d
choi junsu <lezam682@gmail.com>
2021-05-25 16:25:13 +0900

    first commit

---
+++ app/src/main/java/com/jschoi/develop/aop_part03_chapter05/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.jschoi.develop.aop_part03_chapter05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 틴더
+ *
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
